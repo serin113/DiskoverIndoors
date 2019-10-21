3829a12bbb701a56b0c2c546a7f8c1a93ad2dabc
Simon Clemente <serin113@users.noreply.github.com>
2019-10-21 14:18:15 +0800

    Added FontAwesome icons

--- app/src/main/java/com/clemcab/diskoverindoors/MainActivity.java
+++ app/src/main/java/com/clemcab/diskoverindoors/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_qr, R.id.navigation_navigation, R.id.navigation_locations)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
