d58d53791b24f683a4e56fe7d76a2ed495093cb5
rfcabasag <rfcabasag@up.edu.ph>
2019-11-11 03:00:39 +0800

    QR code scanner

--- app/src/main/java/com/clemcab/diskoverindoors/ui/home/HomeFragment.java
+++ app/src/main/java/com/clemcab/diskoverindoors/ui/home/HomeFragment.java
@@ -1,33 +1,104 @@
 package com.clemcab.diskoverindoors.ui.home;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.clemcab.diskoverindoors.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    SurfaceView surfaceView;
+    CameraSource cameraSource;
+    BarcodeDetector barcodeDetector;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        surfaceView = (SurfaceView) root.findViewById(R.id.camerapreview);
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        barcodeDetector = new BarcodeDetector.Builder(getActivity())
+                .setBarcodeFormats(Barcode.QR_CODE).build();
+        cameraSource = new CameraSource.Builder(getActivity(),barcodeDetector)
+                .setRequestedPreviewSize(640,480).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.CAMERA}, 100);
+                    textView.setText("Permission not granted.");
+                    return;
+                } else {
+                    textView.setText("Please focus on QR code.");
+                }
+                try {
+                    cameraSource.start(holder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
             }
         });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+
+                if (qrCodes.size()!=0) {
+                   textView.post(new Runnable() {
+                       @Override
+                       public void run() {
+                           Vibrator vibrator = (Vibrator) getActivity().getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                           textView.setText(qrCodes.valueAt(0).displayValue);
+                       }
+                   });
+                }
+            }
+        });
+
+//        homeViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
