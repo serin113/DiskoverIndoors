11cf63496108d5bdad91c79bb5b4ad0f51e07aba
rfcabasag <rfcabasag@up.edu.ph>
2019-11-18 13:08:49 +0800

    Revert "LineChart for z-axis velocity"
    This reverts commit ca79dd00a78934aeccd04630f01dcfbca7b05db1.

--- app/src/main/java/com/clemcab/diskoverindoors/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/clemcab/diskoverindoors/ui/dashboard/DashboardFragment.java
@@ -1,7 +1,5 @@
 package com.clemcab.diskoverindoors.ui.dashboard;
 
-import android.graphics.Color;
-import android.hardware.SensorEvent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,81 +10,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.clemcab.diskoverindoors.MainActivity;
 import com.clemcab.diskoverindoors.R;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
     private Accelerometer accelerometer;
-    private LineChart mChart;
-    private Thread thread;
-    private boolean plotData = true;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-
-        mChart = (LineChart) root.findViewById(R.id.velocityDataPlot);
-
-// enable description text
-        mChart.getDescription().setEnabled(true);
-
-        // enable touch gestures
-        mChart.setTouchEnabled(true);
-
-        // enable scaling and dragging
-        mChart.setDragEnabled(true);
-        mChart.setScaleEnabled(true);
-        mChart.setDrawGridBackground(false);
-
-        // if disabled, scaling can be done on x- and y-axis separately
-        mChart.setPinchZoom(true);
-
-        // set an alternative background color
-        mChart.setBackgroundColor(Color.WHITE);
-
-        LineData data = new LineData();
-        data.setValueTextColor(Color.WHITE);
-
-        // add empty data
-        mChart.setData(data);
-
-        // get the legend (only possible after setting data)
-        Legend l = mChart.getLegend();
-
-        // modify the legend ...
-        l.setForm(Legend.LegendForm.LINE);
-        l.setTextColor(Color.BLACK);
-
-        XAxis xl = mChart.getXAxis();
-        xl.setTextColor(Color.BLACK);
-        xl.setDrawGridLines(false);
-        xl.setAvoidFirstLastClipping(true);
-        xl.setEnabled(true);
-
-        YAxis leftAxis = mChart.getAxisLeft();
-        leftAxis.setTextColor(Color.BLACK);
-        leftAxis.setDrawGridLines(false);
-        leftAxis.setAxisMaximum(5f);
-        leftAxis.setAxisMinimum(-5f);
-        leftAxis.setDrawGridLines(false);
-
-        YAxis rightAxis = mChart.getAxisRight();
-        rightAxis.setEnabled(false);
-
-        mChart.getAxisLeft().setDrawGridLines(false);
-        mChart.getXAxis().setDrawGridLines(false);
-        mChart.setDrawBorders(false);
+//        dashboardViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
 
         accelerometer = new Accelerometer(getActivity());
         final TextView textViewAccel = root.findViewById(R.id.textViewAccel);
@@ -99,87 +39,10 @@ public class DashboardFragment extends Fragment {
                               "y = " + y + "m/s\n " +
                               "z = " + z + "m/s\n";
                 textViewAccel.setText(text);
-                if(plotData){
-                    addEntry(z);
-                    plotData = false;
-                }
             }
         });
-
-        feedMultiple();
 
         return root;
-    }
-
-
-    private void addEntry(float x) {
-
-        LineData data = mChart.getData();
-
-        if (data != null) {
-
-            ILineDataSet set = data.getDataSetByIndex(0);
-            // set.addEntry(...); // can be called as well
-
-            if (set == null) {
-                set = createSet();
-                data.addDataSet(set);
-            }
-
-//            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 80) + 10f), 0);
-            data.addEntry(new Entry(set.getEntryCount(), x), 0);
-            data.notifyDataChanged();
-
-            // let the chart know it's data has changed
-            mChart.notifyDataSetChanged();
-
-            // limit the number of visible entries
-            mChart.setVisibleXRangeMaximum(150);
-            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
-
-            // move to the latest entry
-            mChart.moveViewToX(data.getEntryCount());
-
-        }
-    }
-
-    private LineDataSet createSet() {
-
-        LineDataSet set = new LineDataSet(null, "Dynamic Data");
-        set.setAxisDependency(YAxis.AxisDependency.LEFT);
-        set.setLineWidth(3f);
-        set.setColor(Color.MAGENTA);
-        set.setHighlightEnabled(false);
-        set.setDrawValues(false);
-        set.setDrawCircles(false);
-        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-        set.setCubicIntensity(0.2f);
-        return set;
-    }
-
-    private void feedMultiple() {
-
-        if (thread != null){
-            thread.interrupt();
-        }
-
-        thread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                while (true){
-                    plotData = true;
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-
-        thread.start();
     }
 
     @Override
@@ -192,17 +55,8 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
-        if (thread != null) {
-            thread.interrupt();
-        }
-        accelerometer.unregister();
-    }
 
-    @Override
-    public void onDestroy() {
         accelerometer.unregister();
-        thread.interrupt();
-        super.onDestroy();
     }
 
 }
