1bd6ebd5c1caa03171aec530b76381c22044b17e
Simon Clemente <serin113@users.noreply.github.com>
2019-11-19 16:16:43 +0800

    Added filters

--- app/src/main/java/com/clemcab/diskoverindoors/ui/dashboard/Accelerometer.java
+++ app/src/main/java/com/clemcab/diskoverindoors/ui/dashboard/Accelerometer.java
@@ -6,12 +6,15 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
+import java.util.AbstractList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Accelerometer {
-    public interface  Listener {
-        void onTranslation(float x_vel,float y_vel, float z_vel, float x, float y, float z);
+    public interface Listener {
+        void onTranslation(float x_vel,float y_vel, float z_vel, float x, float timeDiff);
     }
     private Listener listener;
     public void setListener(Listener l) {
@@ -23,114 +26,134 @@ public class Accelerometer {
     private Sensor magSensor;
     private SensorEventListener sensorEventListener;
 
-    private int sensorBufferMax = 4;
-    private float sensorThresh = 0f;
-    private float velThresh = 0.01f;
-    private int usSamplingDelayAccel = 16667;
+    private float[] gravAccelReading;
+    private float[] magReading;
+    private float[] rotationMatrix;
+    private List<Float> sensorValsX;
+    private List<Float> sensorValsY;
+    private List<Float> sensorValsZ;
+    private long sensorVals_timeStart;
 
-    private final float[] gravAccelReading = new float[3];
-    private final float[] magReading = new float[3];
-    private final float[] rotationMatrix = new float[9];
+    /* SETTINGS */
+    private final int sensorBufferMax   = 4;
+    private final float accelThreshMax  = 3f; // not sure
+    private final float velThreshMin    = 0f;
+    private final float velThreshMax    = 2f; // probably?
+    private final boolean zAxisEnabled  = true;
+    private final boolean groundLock    = true;
+    private int usSamplingDelayAccel    = 16667;
 
-    private Queue<Float> sensorValsX = new LinkedList<>();
-    private Queue<Float> sensorValsY = new LinkedList<>();
-    private Queue<Float> sensorValsZ = new LinkedList<>();
-
-    private float Simp(Float[] vals) {
+    private float Simp(List<Float> vals, float timeDiff) {
         float velocity =
             (
                 (
-                        (float)sensorBufferMax / (1000000f/(float)usSamplingDelayAccel)
+                        (float)sensorBufferMax / (1000000f/timeDiff)
                 )/8f
             ) * (
-                (float)vals[0] + 3f*((float)vals[1]+(float)vals[2]) + (float)vals[3]
+                vals.get(0) + 3f*(vals.get(1)+vals.get(2)) + vals.get(3)
             );
         return velocity;
     }
 
+    private void updateVals(List<Float> vals, float val) {
+        if (vals.size() >= sensorBufferMax)
+            vals.clear();
+        vals.add(val);
+    }
+
     Accelerometer (Context context) {
+        gravAccelReading = new float[3];
+        magReading = new float[3];
+        rotationMatrix = new float[9];
+
+        sensorValsX = new ArrayList<>(sensorBufferMax);
+        sensorValsY = new ArrayList<>(sensorBufferMax);
+        sensorValsZ = new ArrayList<>(sensorBufferMax);
+
         sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         gravAccelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
         sensorEventListener = new SensorEventListener() {
-            private void updateVals(Queue<Float> vals, float val) {
-                if (vals.size() >= sensorBufferMax) {
-                    for (int i = 0; i < vals.size(); i++) {
-                        vals.remove();
-                    }
-                }
-                vals.add(val);
-            }
-
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
                 if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                    System.arraycopy(sensorEvent.values, 0, gravAccelReading,
-                            0, gravAccelReading.length);
+                    if (groundLock) {
+                        System.arraycopy(sensorEvent.values, 0, gravAccelReading,
+                                0, gravAccelReading.length);
+                    }
 
                 } else if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-                    System.arraycopy(sensorEvent.values, 0, magReading,
-                            0, magReading.length);
+                    if (groundLock) {
+                        System.arraycopy(sensorEvent.values, 0, magReading,
+                                0, magReading.length);
+                    }
 
                 } else if (sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+                    if (sensorValsX.size() == sensorBufferMax)
+                        sensorVals_timeStart = System.nanoTime();
+
+                    float x_accel = sensorEvent.values[0];
+                    float y_accel = sensorEvent.values[1];
+                    float z_accel = sensorEvent.values[2];
+
+                    x_accel = x_accel<=accelThreshMax ? x_accel : accelThreshMax;
+                    y_accel = y_accel<=accelThreshMax ? y_accel : accelThreshMax;
+                    z_accel = z_accel<=accelThreshMax ? z_accel : accelThreshMax;
+
                     updateVals(sensorValsX, sensorEvent.values[0]);
                     updateVals(sensorValsY, sensorEvent.values[1]);
                     updateVals(sensorValsZ, sensorEvent.values[2]);
-                    Float[] x = sensorValsX.toArray(new Float[0]);
-                    Float[] y = sensorValsY.toArray(new Float[0]);
-                    Float[] z = sensorValsZ.toArray(new Float[0]);
 
                     if (sensorValsX.size() >= sensorBufferMax) {
-                        SensorManager.getRotationMatrix(rotationMatrix, null, gravAccelReading, magReading);
+                        float timeDiff = ((float)System.nanoTime() - sensorVals_timeStart) / 1000f;
+                        SensorManager.getRotationMatrix(
+                                rotationMatrix,
+                                null,
+                                gravAccelReading,
+                                magReading);
 
-                        float x_avg = 0f;
-                        float y_avg = 0f;
-                        float z_avg = 0f;
+                        float x_vel = Simp(sensorValsX, timeDiff);
+                        float y_vel = Simp(sensorValsY, timeDiff);
+                        float z_vel = Simp(sensorValsZ, timeDiff);
 
-                        for (int i = 0; i < sensorBufferMax; i++) {
-                            x_avg += x[i]*rotationMatrix[0] + y[i]*rotationMatrix[1] + z[i]*rotationMatrix[2];
-                            y_avg += x[i]*rotationMatrix[3] + y[i]*rotationMatrix[4] + z[i]*rotationMatrix[5];
-                            z_avg += x[i]*rotationMatrix[6] + y[i]*rotationMatrix[7] + z[i]*rotationMatrix[8];
-                        }
-                        x_avg /= sensorBufferMax;
-                        y_avg /= sensorBufferMax;
-                        z_avg /= sensorBufferMax;
+                        boolean inThreshVelMin =
+                            Math.abs(x_vel) >= velThreshMin &&
+                            Math.abs(y_vel) >= velThreshMin &&
+                            Math.abs(z_vel) >= velThreshMin;
 
-                        boolean inThresh =
-                            Math.abs(x_avg) >= sensorThresh &&
-                            Math.abs(y_avg) >= sensorThresh &&
-                            Math.abs(z_avg) >= sensorThresh;
-                        float x_vel = Simp(x);
-                        float y_vel = Simp(y);
-                        float z_vel = Simp(z);
-                        if (inThresh) {
-                            boolean inThreshVel =
-                                Math.abs(x_vel) >= velThresh &&
-                                Math.abs(y_vel) >= velThresh &&
-                                Math.abs(z_vel) >= velThresh;
-                            if (inThreshVel) {
-                                x_vel = Simp(x);
-                                y_vel = Simp(y);
-                                z_vel = Simp(z);
-
-                                x_vel = x_vel*rotationMatrix[0] + y_vel*rotationMatrix[1] + z_vel*rotationMatrix[2];
-                                y_vel = x_vel*rotationMatrix[3] + y_vel*rotationMatrix[4] + z_vel*rotationMatrix[5];
-                                z_vel = x_vel*rotationMatrix[6] + y_vel*rotationMatrix[7] + z_vel*rotationMatrix[8];
-
+                        if (inThreshVelMin) {
+                            boolean inThreshVelMax =
+                                Math.abs(x_vel) <= velThreshMax &&
+                                Math.abs(y_vel) <= velThreshMax &&
+                                Math.abs(z_vel) <= velThreshMax;
+                            if (inThreshVelMax) {
+                                if (groundLock) {
+                                    float x_vel_t = x_vel * rotationMatrix[0] + y_vel * rotationMatrix[1] + z_vel * rotationMatrix[2];
+                                    float y_vel_t = x_vel * rotationMatrix[3] + y_vel * rotationMatrix[4] + z_vel * rotationMatrix[5];
+                                    float z_vel_t = x_vel * rotationMatrix[6] + y_vel * rotationMatrix[7] + z_vel * rotationMatrix[8];
+                                    x_vel = x_vel_t;
+                                    y_vel = y_vel_t;
+                                    z_vel = z_vel_t;
+                                }
                             } else {
-                                x_vel = 0f;
-                                y_vel = 0f;
-                                z_vel = 0f;
+                                x_vel = velThreshMax;
+                                y_vel = velThreshMax;
+                                z_vel = velThreshMax;
                             }
                         } else {
                             x_vel = 0f;
                             y_vel = 0f;
                             z_vel = 0f;
                         }
-                        if (listener != null)
-                            listener.onTranslation(x_vel, y_vel, z_vel, x_avg, y_avg, z_avg);
+
+                        if (listener != null) {
+                            if (zAxisEnabled)
+                                listener.onTranslation(x_vel, y_vel, z_vel, x_accel, (1000000f/(float)timeDiff));
+                            else
+                                listener.onTranslation(x_vel, y_vel, 0, x_accel, (1000000f/(float)timeDiff));
+                        }
                     }
                 }
             }
@@ -147,6 +170,16 @@ public class Accelerometer {
             usSamplingDelayAccel = accelSensor.getMinDelay();
         else if (usSamplingDelayAccel > accelSensor.getMaxDelay() && accelSensor.getMaxDelay() > 0)
             usSamplingDelayAccel = accelSensor.getMaxDelay();
+
+        if (gravAccelSensor.getMinDelay() > usSamplingDelayAccel)
+            usSamplingDelayAccel = gravAccelSensor.getMinDelay();
+        else if (usSamplingDelayAccel > gravAccelSensor.getMaxDelay() && gravAccelSensor.getMaxDelay() > 0)
+            usSamplingDelayAccel = gravAccelSensor.getMaxDelay();
+
+        if (magSensor.getMinDelay() > usSamplingDelayAccel)
+            usSamplingDelayAccel = magSensor.getMinDelay();
+        else if (usSamplingDelayAccel > magSensor.getMaxDelay() && magSensor.getMaxDelay() > 0)
+            usSamplingDelayAccel = magSensor.getMaxDelay();
 
         sensorManager.registerListener(sensorEventListener, accelSensor, usSamplingDelayAccel);
         sensorManager.registerListener(sensorEventListener, gravAccelSensor, usSamplingDelayAccel);
--- app/src/main/java/com/clemcab/diskoverindoors/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/clemcab/diskoverindoors/ui/dashboard/DashboardFragment.java
@@ -27,14 +27,12 @@ public class DashboardFragment extends Fragment {
 
         accelerometer.setListener(new Accelerometer.Listener() {
             @Override
-            public void onTranslation(float x_vel, float y_vel, float z_vel, float x, float y, float z) {
+            public void onTranslation(float x_vel, float y_vel, float z_vel, float x_accel, float timeDiff) {
                 String text = "Accelerometer Readings:\n" +
-                              "x_vel = " + x_vel + "m/s\n " +
-                              "y_vel = " + y_vel + "m/s\n " +
-                              "z_vel = " + z_vel + "m/s\n " +
-                              "x = " + x + "m/s\n " +
-                              "y = " + y + "m/s\n " +
-                              "z = " + z + "m/s\n ";
+                              "x_vel = " + x_vel + " m/s\n " +
+                              "y_vel = " + y_vel + " m/s\n " +
+                              "z_vel = " + z_vel + " m/s\n " +
+                              "timeDiff = " + timeDiff + " Hz";
                 textViewAccel.setText(text);
             }
         });
