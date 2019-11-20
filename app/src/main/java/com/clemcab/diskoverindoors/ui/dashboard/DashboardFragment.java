66cc758442dbf0c896a7c3313552d9af2ab000a2
rfcabasag <rfcabasag@up.edu.ph>
2019-11-20 16:37:03 +0800

    added map pointer

--- app/src/main/java/com/clemcab/diskoverindoors/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/clemcab/diskoverindoors/ui/dashboard/DashboardFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,10 +18,14 @@ public class DashboardFragment extends Fragment {
     private DashboardViewModel dashboardViewModel;
     private Accelerometer accelerometer;
 
+    private ImageView map_pointer;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
+
+        map_pointer = root.findViewById(R.id.map_pointer);
 
         accelerometer = new Accelerometer(getActivity());
         final TextView textViewAccel = root.findViewById(R.id.textViewAccel);
@@ -28,11 +33,17 @@ public class DashboardFragment extends Fragment {
         accelerometer.setListener(new Accelerometer.Listener() {
             @Override
             public void onTranslation(float x_vel, float y_vel, float z_vel, float x_accel, float timeDiff) {
+                float x_coord = map_pointer.getX();
+                float y_coord = map_pointer.getY();
                 String text = "Accelerometer Readings:\n" +
                               "x_vel = " + x_vel + " m/s\n " +
                               "y_vel = " + y_vel + " m/s\n " +
                               "z_vel = " + z_vel + " m/s\n " +
-                                "timeDiff = " + timeDiff + " Hz\n ";
+                              "timeDiff = " + timeDiff + " Hz\n " +
+                              "x - " + x_coord + "\n" +
+                              "y - " + y_coord + "\n" ;
+                map_pointer.setX(x_coord - 300*x_vel);
+                map_pointer.setY(y_coord + 300*y_vel);
                 textViewAccel.setText(text);
             }
         });
